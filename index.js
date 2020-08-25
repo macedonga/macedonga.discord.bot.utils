b04063bcf98ea1ee979a4ef0147a329e8e910453
Marco Ceccon <m.ceccon05@gmail.com>
2020-08-25 10:33:03 +0200

    Added shorten command

--- index.js
+++ index.js
@@ -90,6 +90,51 @@ client.on('message', message => {
                 message.channel.send(createError("Error!\n" + err.message));
             });
         }
+    } else if (command === 'shorten') {
+        var url = args[0];
+        var slug = args[1];
+
+        if (url == undefined)
+            return message.channel.send(createError("No URL given!"));
+        var json;
+        if (slug != undefined)
+            json = { url: url, slug: slug }
+        else
+            json = { url: url }
+
+        const data = JSON.stringify(json);
+
+        const options = {
+            hostname: 's.mcdn.ga',
+            path: '/url',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': data.length
+            }
+        };
+        const req = https.request(options, (res) => {
+            let data = '';
+            res.on('data', (chunk) => {
+                data += chunk;
+            });
+            res.on('end', () => {
+                var res = JSON.parse(data);
+                if (data.includes("num")) {
+                    if (res.num === "001")
+                        return message.channel.send(createError("The slug you given is already in use!"));
+                    else if (res.num === "002")
+                        return message.channel.send(createError("No."));
+                    else
+                        return message.channel.send(createError("Invalid slug/URL!"));
+                } else
+                    return message.channel.send(createSuccess("Succesfully shortened the URL " + res.url, "https://s.mcdn.ga/" + res.slug));
+            });
+        }).on("error", (err) => {
+            message.channel.send(createError("Error!\n" + err.message));
+        });
+        req.write(data);
+        req.end();
     }
 });
 
