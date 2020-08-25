dd45a34a1b6c2f0c6daeaedec1dc3160a26f9354
Marco Ceccon <m.ceccon05@gmail.com>
2020-08-25 18:13:41 +0200

    Added "mb.whois" command

--- index.js
+++ index.js
@@ -67,6 +67,7 @@ client.on('message', message => {
                     .setTitle(res.title)
                     .setURL(res.postLink)
                     .setImage(res.url)
+                    .setTimestamp()
                     .setFooter('Made by macedonga#5526', 'https://cdn.macedon.ga/p.n.g.r.png');
 
                 message.channel.send(embed);
@@ -157,10 +158,68 @@ client.on('message', message => {
             .setColor('#0000ff')
             .setTitle("Help")
             .setDescription("*The arguments inside `<>` are not required.*")
-            .addFields({ name: '**`mb.ban @user <DAYS>`**', value: 'Bans a member.\n`<DAYS>`: Must be a value from 1 to 7. Default is 7.\n*You need the `BAN_MEMBERS` permission.*' }, { name: '**`mb.kick @user`**', value: 'Kick a member.\n*You need the `KICK_MEMBERS` permission.*' }, { name: '**`mb.purge 1-99`**', value: 'Purges messages in the channel.\n*You need the `MANAGE_MESSAGES` permission.*' }, { name: '**`mb.meme`**', value: 'Returns a meme from `r/memes`, `r/dankmemes` or `r/meirl`' }, { name: '**`mb.insult @user`**', value: 'Insults the specified user.' }, { name: '**`mb.shorten https://link.com <SLUG>`**', value: 'Shortens the given URL.\n`<SLUG>`: the id of the shortened URL.' })
+            .addFields({ name: '**`mb.ban @user <DAYS>`**', value: 'Bans a member.\n`<DAYS>`: Must be a value from 1 to 7. Default is 7.\n*You need the `BAN_MEMBERS` permission.*' }, { name: '**`mb.kick @user`**', value: 'Kick a member.\n*You need the `KICK_MEMBERS` permission.*' }, { name: '**`mb.purge 1-99`**', value: 'Purges messages in the channel.\n*You need the `MANAGE_MESSAGES` permission.*' }, { name: '**`mb.meme`**', value: 'Returns a meme from `r/memes`, `r/dankmemes` or `r/meirl`' }, { name: '**`mb.insult @user`**', value: 'Insults the specified user.' }, { name: '**`mb.shorten https://link.com <SLUG>`**', value: 'Shortens the given URL.\n`<SLUG>`: the id of the shortened URL.' }, { name: '**`mb.whois https://link.com <dbg.true>`**', value: 'Returns WHOIS info about the domain.\n`<dbg.true>`: returns the json response from the API. Debugging purposes only!' })
             .setTimestamp()
             .setFooter('Made by macedonga#5526', 'https://cdn.macedon.ga/p.n.g.r.png');
         message.channel.send(embed);
+    } else if (command === 'whois') {
+        if (args[0] != undefined) {
+            var domain = args[0];
+            https.get('https://api.ip2whois.com/v1?key=free&domain=' + domain, (resp) => {
+                let data = '';
+                resp.on('data', (chunk) => {
+                    data += chunk;
+                });
+                resp.on('end', () => {
+                    var res = JSON.parse(data);
+                    if (res.error_code != "")
+                        return message.channel.send(createError("The API returned an error!\n`" + res.error_message + "`"));
+                    else {
+                        const embed = new Discord.MessageEmbed()
+                            .setTitle("WHOIS info for " + domain)
+                            .setDescription('By https://ip2whois.com/ • *`NA` means "Not Available"*')
+                            .addField('Domain ID', res.domain_id || "NA")
+                            .addField('Status', res.status || "NA")
+                            .addField('\u200b', '\u200b')
+                            .addField('Created', res.create_date || "NA", true)
+                            .addField('Update date', res.update_date || "NA", true)
+                            .addField('Expiring date', res.expire_date || "NA", true)
+                            .addField('Domain age', res.domain_age || "NA", true)
+                            .addField('\u200b', '\u200b')
+                            .addField('Nameservers', res.nameservers || "NA", true);
+                        if (res.registrant != undefined) {
+                            embed.addField('\u200b', '\u200b')
+                                .addField('Registrant\'s name', res.registrant.name || "NA", true)
+                                .addField('Registrant\'s organization', res.registrant.organization || "NA", true)
+                                .addField('Registrant\'s street address', res.registrant.street_address || "NA", true)
+                                .addField('Registrant\'s city', res.registrant.city || "NA", true)
+                                .addField('Registrant\'s region', res.registrant.region || "NA", true)
+                                .addField('Registrant\'s zip code', res.registrant.zip_code || "NA", true)
+                                .addField('Registrant\'s country', res.registrant.country || "NA", true)
+                                .addField('Registrant\'s phone', res.registrant.phone || "NA", true)
+                                .addField('Registrant\'s fax', res.registrant.fax || "NA", true)
+                                .addField('Registrant\'s email', res.registrant.email || "NA", true)
+                        }
+                        if (res.registrar != undefined) {
+                            embed.addField('\u200b', '\u200b')
+                                .addField('Registrar\'s IANA ID', res.registrar.iana_id || "NA", true)
+                                .addField('Registrar\'s Name', res.registrar.name || "NA", true)
+                                .addField('Registrar\'s URL', res.registrar.url || "NA", true)
+                        }
+                        embed.setTimestamp()
+                            .setFooter('Made by macedonga#5526', 'https://cdn.macedon.ga/p.n.g.r.png');
+                        message.channel.send(embed);
+                    }
+                    if (args[1] == "dbg.true")
+                        message.channel.send(data);
+                });
+
+            }).on("error", (err) => {
+                message.channel.send(createError("Error!\n" + err.message));
+            });
+        } else {
+            return message.channel.send(createError("No URL given!"));
+        }
     }
 });
 
