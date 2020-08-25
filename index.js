9d811c87b193bfa326e3491fafa45540e9ec8413
Marco Ceccon <m.ceccon05@gmail.com>
2020-08-25 09:40:10 +0200

    Fixed banning and kicking.

--- index.js
+++ index.js
@@ -15,78 +15,51 @@ client.on('message', message => {
     if (command === 'kick') {
         if (message.member.hasPermission("KICK_MEMBERS")) {
             if (!message.mentions.users.size) {
-                const embed = new Discord.MessageEmbed()
-                    .setColor('#ff0000')
-                    .setTitle('Error!')
-                    .setDescription('You didn\'t told me who to kick!')
-                    .setTimestamp()
-                return message.channel.send(embed);
+                return message.channel.send(createWarning('You didn\'t told me who to kick!'));
             } else {
                 const taggedUser = message.mentions.users.first();
-                try {
-                    const targetMember = message.guild.members.cache.get(taggedUser.id);
-                    targetMember.kick();
-                } catch (error) {
-                    const embed = new Discord.MessageEmbed()
-                        .setColor('#ff0000')
-                        .setTitle('Error!')
-                        .setDescription('I cannot kick ' + taggedUser.username)
-                        .addFields({ name: 'Error', value: error })
-                        .setTimestamp()
-                    return message.channel.send(embed);
-                }
+                const targetMember = message.guild.members.cache.get(taggedUser.id);
+                targetMember.kick();
+                setTimeout(function() {
+                    if (message.guild.member(taggedUser.id)) {
+                        return message.channel.send(createError('An error occured while kicking ' + taggedUser.username));
+                    } else {
+                        return message.channel.send(createSuccess('Succesfully kicked ' + taggedUser.username + '!', ''));
+                    }
+                }, 500);
             }
         } else {
-            const embed = new Discord.MessageEmbed()
-                .setColor('#ff0000')
-                .setTitle('Error!')
-                .setDescription('You cannot kick ' + taggedUser.username)
-                .setTimestamp()
-            return message.channel.send(embed);
+            return message.channel.send(createWarning('You cannot kick!'));
         }
     } else if (command === 'ban') {
         if (message.member.hasPermission("BAN_MEMBERS")) {
             if (!message.mentions.users.size) {
-                const embed = new Discord.MessageEmbed()
-                    .setColor('#ff0000')
-                    .setTitle('Error!')
-                    .setDescription('You didn\'t told me who to ban!')
-                    .setTimestamp()
-                return message.channel.send(embed);
+                return message.channel.send(createWarning('You didn\'t told me who to ban!'));
             } else {
                 const taggedUser = message.mentions.users.first();
-                try {
-                    const targetMember = message.guild.members.cache.get(taggedUser.id);
-                    var days = args[1] || 7;
-                    targetMember.ban({ days: days });
-                } catch (error) {
-                    const embed = new Discord.MessageEmbed()
-                        .setColor('#ff0000')
-                        .setTitle('Error!')
-                        .setDescription('I cannot ban ' + taggedUser.username)
-                        .addFields({ name: 'Error', value: error })
-                        .setTimestamp()
-                    return message.channel.send(embed);
+                const targetMember = message.guild.members.cache.get(taggedUser.id);
+                var days = parseInt(args[0]);
+                if (isNaN(days)) {
+                    days = 7;
                 }
+                targetMember.ban({ days: days });
+                setTimeout(function() {
+                    if (message.guild.member(taggedUser.id)) {
+                        return message.channel.send(createError('An error occured while banning ' + taggedUser.username));
+                    } else {
+                        return message.channel.send(createSuccess('Succesfully banned ' + taggedUser.username + '!', ''));
+                    }
+                }, 500);
             }
         } else {
-            const embed = new Discord.MessageEmbed()
-                .setColor('#ff0000')
-                .setTitle('Error!')
-                .setDescription('You cannot ban ' + taggedUser.username)
-                .setTimestamp()
-            return message.channel.send(embed);
+            return message.channel.send(createWarning('You cannot ban!'));
         }
     } else if (command === 'meme') {
         https.get('https://api1-funtext.herokuapp.com/rndmemes', (resp) => {
             let data = '';
-
-            // A chunk of data has been recieved.
             resp.on('data', (chunk) => {
                 data += chunk;
             });
-
-            // The whole response has been received. Print out the result.
             resp.on('end', () => {
                 var res = JSON.parse(data);
                 const embed = new Discord.MessageEmbed()
@@ -98,37 +71,54 @@ client.on('message', message => {
             });
 
         }).on("error", (err) => {
-            console.log("Error: " + err.message);
+            message.channel.send(createError("Error!\n" + err.message));
         });
     } else if (command === 'insult') {
         if (!message.mentions.users.size) {
-            const embed = new Discord.MessageEmbed()
-                .setColor('#ff0000')
-                .setTitle('Error!')
-                .setDescription('You didn\'t told me who to insult!')
-                .setTimestamp()
-            return message.channel.send(embed);
+            return message.channel.send(createWarning("You didn't told me who to insult!"));
         } else {
             https.get('https://insult.mattbas.org/api/insult', (resp) => {
                 let data = '';
-
-                // A chunk of data has been recieved.
                 resp.on('data', (chunk) => {
                     data += chunk;
                 });
-
-                // The whole response has been received. Print out the result.
                 resp.on('end', () => {
                     const taggedUser = message.mentions.users.first();
                     message.channel.send(taggedUser.username + " " + data.toLowerCase() + ".");
                 });
-
             }).on("error", (err) => {
-                console.log("Error: " + err.message);
+                message.channel.send(createError("Error!\n" + err.message));
             });
         }
     }
 });
+
+function createError(error) {
+    const embed = new Discord.MessageEmbed()
+        .setColor('#ff0000')
+        .setTitle('Error!')
+        .setDescription(error)
+        .setTimestamp()
+    return embed;
+}
+
+function createWarning(warning) {
+    const embed = new Discord.MessageEmbed()
+        .setColor('#ffff00')
+        .setTitle('Warning!')
+        .setDescription(warning)
+        .setTimestamp()
+    return embed;
+}
+
+function createSuccess(title, success) {
+    const embed = new Discord.MessageEmbed()
+        .setColor('#00ff00')
+        .setTitle(title)
+        .setDescription(success)
+        .setTimestamp()
+    return embed;
+}
 
 client.on('guildMemberRemove', member => {
     if (member.guild.id === 736591510413508619) {
