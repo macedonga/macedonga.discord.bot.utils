cbd50c8c83c42f1afa349358b4c1c0b543408085
Marco Ceccon <m.ceccon05@gmail.com>
2020-08-28 21:28:36 +0200

    Added welcome and goodbye message

--- index.js
+++ index.js
@@ -346,12 +346,13 @@ client.on('message', message => {
             mes.react("👎");
             message.delete();
         });
-    } else if (neuralnetwork.isQuestion(message.content)) {
+    }
+    /*else if (neuralnetwork.isQuestion(message.content)) {
         const lmgtfy = new URL("https://lmgtfy.com/");
         lmgtfy.searchParams.append("q", message.content);
         lmgtfy.searchParams.append("s", "d");
         message.channel.send(lmgtfy.href);
-    }
+    }*/
 });
 
 client.on('guildMemberRemove', member => {
@@ -376,8 +377,74 @@ client.on('guildMemberRemove', member => {
 
         const channel = member.guild.channels.cache.get('738665053544251412');
         return channel.send(createSuccess("Deleted `" + member.user.username + "` messages successfully!", ""));
+    } else if (member.guild.id === "748921083461107794") {
+        const welcome = member.guild.channels.cache.get("748980052837924895");
+
+        var ran = randomRange(0, 4);
+        var message;
+        switch (ran) {
+            case 0:
+                message = "Bye bye, <@!" + member.user + ">.";
+                break;
+            case 1:
+                message = "F in the chat. <@!" + member.user + "> left.";
+                break;
+            case 2:
+                message = "So long ||gay|| bowser!\nUser <@!" + member.user + "> left.";
+                break;
+            case 3:
+                message = "Oof,  <@!" + member.user + "> left.";
+                break;
+            case 4:
+                message = "I feel bad for <@!" + member.user + ">, he doesn't know what's he's missing!";
+                break;
+        }
+        const embed = new Discord.MessageEmbed()
+            .setColor('#00ff00')
+            .setTitle("Noo, an user left! 👋")
+            .setDescription(message)
+            .setTimestamp()
+            .setFooter('Made by macedonga#5526', 'https://cdn.macedon.ga/p.n.g.r.png');
+        return welcome.send(embed);
     }
 });
 
-neuralnetwork.init();
+client.on('guildMemberAdd', member => {
+    if (member.guild.id === "748921083461107794") {
+        const welcome = member.guild.channels.cache.get("748980052837924895");
+
+        var ran = randomRange(0, 4);
+        var greeting;
+        switch (ran) {
+            case 0:
+                greeting = "Brace yourselves, <@!" + member.user + "> just joined the server!\nBe sure to read the rules!";
+                break;
+            case 1:
+                greeting = "Challenger approaching - <@!" + member.user + "> has appeared!\nBe sure to read the rules!";
+                break;
+            case 2:
+                greeting = "Welcome <@!" + member.user + ">. Leave your weapon by the door.\nBe sure to read the rules!";
+                break;
+            case 3:
+                greeting = "I hope you got lots of memes <@!" + member.user + ">!\nBe sure to read the rules!";
+                break;
+            case 4:
+                greeting = "A wild <@!" + member.user + "> appeared!\nBe sure to read the rules!";
+                break;
+        }
+        const embed = new Discord.MessageEmbed()
+            .setColor('#00ff00')
+            .setTitle("Hey! 👋 - A new user arrived!")
+            .setDescription(greeting)
+            .setTimestamp()
+            .setFooter('Made by macedonga#5526', 'https://cdn.macedon.ga/p.n.g.r.png');
+        return welcome.send(embed);
+    }
+});
+
+function randomRange(min, max) { // returns an int >= min and <= max
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//neuralnetwork.init();
 client.login(process.env.TOKEN);
