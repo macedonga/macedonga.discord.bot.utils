878c564f7e53b114d899e3e8bfb7a4235a3e178f
Marco Ceccon <m.ceccon05@gmail.com>
2020-09-03 18:18:12 +0200

    Added review command

---
+++ commands/review.js
@@ -0,0 +1,36 @@
+const Discord = require('discord.js');
+const { settings } = require('../data/variables');
+const { createError, createWarning, createSuccess } = require('../utils/functions');
+
+module.exports = {
+    name: 'review',
+    description: 'Reviews server.',
+    guildOnly: true,
+    execute(message, args) {
+        if (settings[message.guild.id][0].review != null) {
+            const stars = parseInt(args[0]);
+            if (isNaN(stars) || stars > 5)
+                return message.channel.send(createError("Not a valid number!\nMake sure it's a number from 0 to 5"));
+            var review = args.splice(1).join(" ");
+            if (review === "")
+                return message.channel.send(createError("No review written"));
+            let starRating = "Star rating: "
+            for (var i = 0; i < stars; i++)
+                starRating += "⭐";
+
+            const embed = new Discord.MessageEmbed()
+                .setColor('#0000ff')
+                .setAuthor(message.member.user.username, message.member.user.avatarURL())
+                .setTitle("New review")
+                .addField(review, '\u200B')
+                .setTimestamp()
+                .setFooter(starRating, "https://cdn.macedon.ga/p.n.g.r.png");
+
+            const reviews = message.member.guild.channels.cache.get(settings[message.member.guild.id][0].review.id);
+
+            reviews.send(embed);
+            return message.channel.send(createSuccess("Success!", "Review sent!"));
+        } else
+            return message.channel.send(createWarning("The owner didn't configured reviews for this server."));
+    },
+};
--- commands/settings.get.js
+++ commands/settings.get.js
@@ -11,20 +11,34 @@ module.exports = {
             .setTitle('The settings for this server are:');
         if (settings[message.guild.id][0].lmgtfy === "true")
             embed.addField('Is LMGTFY activated?', "`YES`");
-        else
-            embed.addField('Is LMGTFY activated?', "`NO`");
+        else {
+            embed.addField('\u200B', '\u200B')
+                .addField('Is LMGTFY activated?', "`NO`");
+        }
         if (settings[message.guild.id][0].wm != null)
             embed.addField('\u200B', '\u200B')
             .addField('Are goodbye/welcome messages activated?', "`YES`", true)
             .addField('In which channel?', `\`${settings[message.guild.id][0].wm.name}\``, true)
-        else
-            embed.addField('Are goodbye/welcome messages activated?', "`NO`");
+        else {
+            embed.addField('\u200B', '\u200B')
+                .addField('Are goodbye/welcome messages activated?', "`NO`");
+        }
         if (settings[message.guild.id][0].listing != null)
             embed.addField('\u200B', '\u200B')
             .addField('Is "Listing" enabled for this server?', "`YES`", true)
             .addField('In which category?', `\`${settings[message.guild.id][0].listing.name}\``, true)
-        else
-            embed.addField('Is "Listing" enabled for this server?', "`NO`");
+        else {
+            embed.addField('\u200B', '\u200B')
+                .addField('Is "Listing" enabled for this server?', "`NO`");
+        }
+        if (settings[message.guild.id][0].review != null)
+            embed.addField('\u200B', '\u200B')
+            .addField('Are reviews enabled for this server?', "`YES`", true)
+            .addField('In which channel?', `\`${settings[message.guild.id][0].review.name}\``, true)
+        else {
+            embed.addField('\u200B', '\u200B')
+                .addField('Are reviews enabled for this server?', "`NO`");
+        }
         embed.setTimestamp()
             .setFooter('Made by macedonga#5526', 'https://cdn.macedon.ga/p.n.g.r.png');
         message.channel.send(embed);
