411e00129328de39ac929d450b021c4c354ed818
Marco Ceccon <m.ceccon05@gmail.com>
2020-09-06 17:49:26 +0200

    Fixed sockets

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const Discord = require('discord.js');
 
 require('dotenv').config()
 const neuralnetwork = require('./utils/neural.network');
-const sockets = require('./functions/socket.io');
+
+var request = require('request');
+var socket = require('socket.io-client')('https://api.macedon.ga');
+const { settings } = require('./data/variables');
 
 const fs = require('fs');
 
@@ -23,6 +26,47 @@ for (const file of eventFiles) {
     console.log(`Event loaded: ${file}`);
 }
 
-sockets.SetSocket();
+socket.on('settings update', function(data) {
+    request.post('https://api.macedon.ga/mdbu/settings/get', { json: { sid: data } }, function(error, response, body) {
+        if (body[0].sid) {
+            settings[data] = [];
+            settings[data].push(body[0]);
+        }
+    });
+});
+
+socket.on('get channels', function(data) {
+    try {
+        var guild = client.guilds.cache.get(data);
+        var ch = {};
+        guild.channels.cache.forEach(channel => {
+            if (channel.type === "text")
+                ch[channel.id] = {
+                    name: channel.name
+                };
+        });
+        socket.emit('return channels', ch);
+    } catch {
+        socket.emit('error');
+    }
+});
+
+socket.on('get categories', function(data) {
+    try {
+        var guild = client.guilds.cache.get(data);
+        var ch = {};
+
+        guild.channels.cache.forEach(channel => {
+            if (channel.type === "category")
+                ch[channel.id] = {
+                    name: channel.name
+                };
+        });
+        socket.emit('return categories', ch);
+    } catch {
+        socket.emit('error');
+    }
+});
+
 neuralnetwork.init();
 client.login(process.env.TOKEN);
